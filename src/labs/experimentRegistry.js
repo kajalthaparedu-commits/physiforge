8c356b9932a06388755e1d6e148169846d4cfe5c
kajalthaparedu-commits <kajal.thapar.edu@gmail.com>
2026-03-08 22:13:19 +0530

    Add experiment registry

---
+++ src/labs/experimentRegistry.js
@@ -0,0 +1,31 @@
+export const experiments = {
+
+electrostatics: [
+
+{
+name: "Electric Field Football",
+icon: "⚽",
+file: "../../simulations/electric-field-football.html"
+},
+
+{
+name: "Field Line Visualizer",
+icon: "📡",
+file: null
+},
+
+{
+name: "Equipotential Mapper",
+icon: "🟢",
+file: null
+},
+
+{
+name: "Gauss Law Explorer",
+icon: "∮",
+file: null
+}
+
+]
+
+}
